0a4ee637bdb3d6b1f8ff7da209cec2d4a3125816
Vishal Bansal <vishal.bansal@berkeley.edu>
2022-07-07 12:15:12 -0700

    String[] -> String

--- src/Pair.ts
+++ src/Pair.ts
@@ -1,6 +1,7 @@
 export class Pair {
     key: string
     value: string
+    readonly _amount: number
 
     constructor (key: string, value: string) {
         this.key = key
--- src/Query.ts
+++ src/Query.ts
@@ -1,23 +1,24 @@
 import {Pair} from './Pair'
-
 interface IQuery {
-    get(key: string) : Promise<Pair []>
-    set(key: string, value: string)
+    get(key: string) : Promise<string>
+    set(key: string, value: string): Promise<Pair>
 }
-
 export class Query implements IQuery {
     computer: any
-    pair: Pair
     constructor (computer: any) {
         this.computer = computer
     }
-    async get(key: string) {
-        const revs = await this.computer.queryRevs({key})
-        const objects: Pair[] = await Promise.all(revs.map((rev) => this.computer.sync(rev)))
-        return objects
+    async get(key: string): Promise<string> {
+        const revs = await this.computer.queryRevs({ contract: Pair })
+        const pairs: Pair[] = await Promise.all(revs.map((rev) => this.computer.sync(rev)))
+        pairs.filter((pair) => pair.key === key).map((pair) => pair.value)
+        const max = Math.max.apply(Math, pairs.map((pair) => pair._amount))
+        return pairs.find((pair) => pair._amount === max).key
+    }  
+    // todo: we want the type of get to be (key: string): Promise<string>
+    // One way to do that is to return the object that has the highest _amount
+    async set(key: string, value: string): Promise<Pair> {
+        return this.computer.new(Pair, [key, value])
     }
-    set(key: string, value: string) {
-        this.pair = this.computer.new(Pair, [key, value])
-        return this.pair
-    }
+      
 }
